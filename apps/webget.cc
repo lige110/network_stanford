97331e37d55f5df72d554c41a9df7de3a1786c46
lige110 <meng-li-2020@outlook.com>
2022-03-27 09:10:39 +0000

    lab0 finished

--- apps/webget.cc
+++ apps/webget.cc
@@ -7,18 +7,19 @@
 using namespace std;
 
 void get_URL(const string &host, const string &path) {
-    // Your code here.
+    
+    TCPSocket sock1;
+    sock1.connect(Address(host, "http"));
+    string mes = "GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n";
+    sock1.write(mes);
+    while (!sock1.eof())
+    {
+        cout << sock1.read();
+    }
+    sock1.close();
 
-    // You will need to connect to the "http" service on
-    // the computer whose name is in the "host" string,
-    // then request the URL path given in the "path" string.
-
-    // Then you'll need to print out everything the server sends back,
-    // (not just one call to read() -- everything) until you reach
-    // the "eof" (end of file).
-
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    cerr << "Function called once: get_URL(" << host << ", " << path << ").\n"     ;
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -12,42 +12,72 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
+ByteStream::ByteStream(const size_t capacity) { _capacity = capacity; }
 
 size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    size_t length = min(data.length(), remaining_capacity());
+    for (size_t i = 0; i < length; i++)
+    {
+        _buffer.push_back(data[i]);
+        _toltal_written ++;
+    }
+    return length;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    size_t output_len = min(len, buffer_size());
+    auto it = _buffer.begin();
+    string result = "";
+
+    for (size_t i = 0; i < output_len; i++)
+    {
+        result += *it;
+        it++;
+    }
+
+    return result;
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
+void ByteStream::pop_output(const size_t len) { 
+    size_t output_len = min(len, buffer_size());
+    for (size_t i = 0; i < output_len; i++)
+    {
+        _buffer.pop_front();
+        _total_popped ++;
+    }
+ }
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+    size_t output_len = min(len, buffer_size());
+    string result = "";
+
+    for (size_t i = 0; i < output_len; i++)
+    {
+        result += _buffer.front();
+        _buffer.pop_back();
+        _total_popped ++;
+    }
+
+    return result;
 }
 
-void ByteStream::end_input() {}
+void ByteStream::end_input() { _input_ended_flag = true; }
 
-bool ByteStream::input_ended() const { return {}; }
+bool ByteStream::input_ended() const { return _input_ended_flag; }
 
-size_t ByteStream::buffer_size() const { return {}; }
+size_t ByteStream::buffer_size() const { return _buffer.size(); }
 
-bool ByteStream::buffer_empty() const { return {}; }
+bool ByteStream::buffer_empty() const { return _buffer.empty(); }
 
-bool ByteStream::eof() const { return false; }
+bool ByteStream::eof() const { return buffer_empty() && input_ended(); }
 
-size_t ByteStream::bytes_written() const { return {}; }
+size_t ByteStream::bytes_written() const { return _toltal_written; }
 
-size_t ByteStream::bytes_read() const { return {}; }
+size_t ByteStream::bytes_read() const { return _total_popped; }
 
-size_t ByteStream::remaining_capacity() const { return {}; }
+size_t ByteStream::remaining_capacity() const { return _capacity - _buffer.size(); }
